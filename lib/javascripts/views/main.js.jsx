2b6523458a3e57a686bf059ec0136dcf5c300537
Jesse Stuart <jesse@jessestuart.ca>
2014-07-24 12:10:58 -0400

    App skeleton

---
+++ lib/javascripts/views/main.js.jsx
@@ -0,0 +1,15 @@
+/** @jsx React.DOM */
+
+(function () {
+
+"use strict";
+
+Nike.Views.Main = React.createClass({
+	displayName: "Nike.Views.Main",
+
+	render: function () {
+		return null;
+	}
+});
+
+})();
