452ede318ccbbc21f1a6b03db84716fef6a1ce15
Jesse Stuart <jesse@jessestuart.ca>
2014-07-24 15:25:56 -0400

    Refactor tasks list into store

--- lib/javascripts/views/main.js.jsx
+++ lib/javascripts/views/main.js.jsx
@@ -1,27 +1,51 @@
 /** @jsx React.DOM */
 //= require ./tasks-list
+//= require ../stores/tasks
 
 (function () {
 
 "use strict";
 
+var TasksStore = Nike.Stores.Tasks;
+
+function getState() {
+	var state = {
+		tasksStoreId: "main"
+	};
+
+	state.tasks = TasksStore.getTasks(state.tasksStoreId);
+
+	return state;
+}
+
 Nike.Views.Main = React.createClass({
 	displayName: "Nike.Views.Main",
 
 	render: function () {
-		var tasks = [];
-		for (var i = 1; i <= 100; i++) {
-			tasks.push({
-				id: "task-"+ i,
-				name: "task "+ i
-			});
-		}
-
+		var tasks = this.state.tasks;
 		return (
 			<section>
 				<Nike.Views.TasksList tasks={tasks} />
 			</section>
 		);
+	},
+
+	getInitialState: function () {
+		return getState(this.props);
+	},
+
+	componentDidMount: function () {
+		var storeId = this.state.tasksStoreId;
+		TasksStore.addChangeListener(storeId, this.__handleTasksStoreChange);
+	},
+
+	componentWillUnmount: function () {
+		var storeId = this.state.tasksStoreId;
+		TasksStore.removeChangeListener(storeId, this.__handleTasksStoreChange);
+	},
+
+	__handleTasksStoreChange: function () {
+		this.setState(getState(this.props));
 	}
 });
 
