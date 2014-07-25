ab8a58b0db512320ffa50369d4fa441d3a138edc
Jesse Stuart <jesse@jessestuart.ca>
2014-07-25 15:27:07 -0400

    Add ability to mark tasks complete

--- lib/javascripts/views/task.js.jsx
+++ lib/javascripts/views/task.js.jsx
@@ -1,8 +1,11 @@
 /** @jsx React.DOM */
+//= require ../actions/task
 
 (function () {
 
 "use strict";
+
+var TaskActions = Nike.Actions.Task;
 
 Nike.Views.Task = React.createClass({
 	displayName: "Nike.Views.Task",
@@ -10,13 +13,21 @@ Nike.Views.Task = React.createClass({
 	render: function () {
 		var task = this.props.task;
 		return (
-			<article onClick={this.__handleClick}>
+			<article onClick={this.__handleClick} className={"task"+ (task.complete ? " complete" : "")}>
+				<input type="checkbox" ref="checkbox" onChange={this.__handleCheckedChange} checked={task.complete} />
 				{task.name}
 			</article>
 		);
 	},
 
+	__handleCheckedChange: function (e) {
+		TaskActions.setTaskCompletion(this.props.task.id, e.target.checked);
+	},
+
 	__handleClick: function (e) {
+		if (e.target === this.refs.checkbox.getDOMNode()) {
+			return;
+		}
 		e.preventDefault();
 		this.props.onSelected(this.props.task);
 	}
